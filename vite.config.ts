178a9a445c46fa207a9e79d2c9976a5f1cb04a90
sumanthreddyboyalla01 <oksumanth@gmail.com>
2026-07-04 16:17:16 +0530

    Fix Vercel deployment config using nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
+import { nitro } from "nitro/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
@@ -12,6 +12,9 @@ export default defineConfig({
   },
   plugins: [
     tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
     react(),
     tailwindcss(),
     visualizer({ open: false, gzipSize: true, brotliSize: true }),
